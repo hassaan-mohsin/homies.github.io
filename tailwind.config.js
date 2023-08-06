e503ce7ba7f0aa522228ea5c7247921dca93375a
hassaan-mohsin <125756306+hassaan-mohsin@users.noreply.github.com>
2023-08-06 20:46:45 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    
+
+    extend: {
+      colors: {
+        customColor: '#6DD6F7', // Replace 'customColor' with a name of your choice
+      },
+    },
+  },
+  plugins: [],
+}
+
